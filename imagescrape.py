be82a3e26e40e8749ca2d88e47143eba758caaa8
Shashwatbokhad <111687416+Shashwatbokhad@users.noreply.github.com>
2023-06-07 03:19:28 -0700

    Update imagescrape.py

--- imagescrape.py
+++ imagescrape.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url= "https://www.google.com/search?q=cat&tbm=isch&sxsrf=APwXEdfZxzvQOglsxjqEGJ4KCldb4q-TPw%3A1685895788758&source=hp&biw=1366&bih=625&ei=bLp8ZJ-RLL6VseMPgISt8AY&iflsig=AOEireoAAAAAZHzIfNfADnf8pH88pd-KLF1O0av6nM0R&ved=0ahUKEwif9_Wcg6r_AhW-SmwGHQBCC24Q4dUDCAc&uact=5&oq=dog&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyBQgAEIAEMggIABCABBCxAzIICAAQgAQQsQM6BwgjEOoCECc6BAgjECdQpA1YpBFg2hJoAXAAeACAAcQBiAGGA5IBAzAuMpgBAKABAaoBC2d3cy13aXotaW1nsAEK&sclient=img"
-
+main_d = os.getcwd()
 def image_scrapper(url,key):
     r = requests.get(url)
     htmldata = r.text
@@ -40,5 +40,6 @@ while(1):
     url = "https://www.google.com/search?q="+ query +"&tbm=isch&sxsrf=APwXEdfZxzvQOglsxjqEGJ4KCldb4q-TPw%3A1685895788758&source=hp&biw=1366&bih=625&ei=bLp8ZJ-RLL6VseMPgISt8AY&iflsig=AOEireoAAAAAZHzIfNfADnf8pH88pd-KLF1O0av6nM0R&ved=0ahUKEwif9_Wcg6r_AhW-SmwGHQBCC24Q4dUDCAc&uact=5&oq=dog&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyBQgAEIAEMggIABCABBCxAzIICAAQgAQQsQM6BwgjEOoCECc6BAgjECdQpA1YpBFg2hJoAXAAeACAAcQBiAGGA5IBAzAuMpgBAKABAaoBC2d3cy13aXotaW1nsAEK&sclient=img"
     key = query
     image_scrapper(url,key)
+    os.chdir(main_d)
 
 
